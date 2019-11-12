430772740556512227470b5c0a7bcba6bcba2a19
Michael Walker <mike@barrucadu.co.uk>
2019-11-12 22:15:37 +0000

    Rewrite namespace deploy script
    - Copy over only the required apps
    - Fill in the EXTERNAL_DOMAIN_NAME
    - Get the jumpbox IP from terraform automatically

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env bash
 
 NAMESPACE="$1"
-JUMPBOX="$2"
 
 HERE="$(git rev-parse --show-toplevel)/kubernetes"
 cd "$HERE"
 
-if [[ -z "$NAMESPACE" ]] || [[ -z "$JUMPBOX" ]] || [[ ! -e "${NAMESPACE}/deploy.sh" ]]; then
-    echo "usage: $0 <namespace> <jumpbox ip>"
+if [[ -z "$NAMESPACE" ]] || [[ ! -e "${NAMESPACE}/secrets.yaml.template" ]]; then
+    echo "usage: $0 <namespace>"
     exit 1
 fi
 
-set -e
+cd "$(git rev-parse --show-toplevel)/provisioning/terraform"
+JUMPBOX="$(terraform output public-ssh-ip)"
+
+cd "$HERE"
+
+source ../config
+
+pushd "$NAMESPACE"
+cp secrets.yaml.template secrets.yaml
+sed -i "s/EXTERNAL_DOMAIN_NAME/${EXTERNAL_DOMAIN_NAME}/" secrets.yaml
+for service in *.service.yaml; do
+  app="${service//.service.yaml/}"
+  cp "../apps/${app}.deployment.yaml" .
+done
+popd
 
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r "$NAMESPACE" "root@${JUMPBOX}:"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A "root@${JUMPBOX}" "./${NAMESPACE}/deploy.sh"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A "root@${JUMPBOX}" <<EOF
+set -ex
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r "$NAMESPACE" k8s-master.govuk-k8s.test:
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no k8s-master.govuk-k8s.test <<INNER
+set -ex
+source .bashrc
+for file in ${NAMESPACE}/*.yaml; do
+  kubectl --namespace "$NAMESPACE" apply -f \\\$file
+done
+INNER
+EOF
